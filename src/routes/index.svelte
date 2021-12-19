b8bb32e5b9c49e9103862ca2083a98402fcc4976
ReeyaB123 <reeya4151@gmail.com>
2021-12-19 02:44:32 +0000

    commit 1

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script>
+    let temperature = "";
+    let location = "";
+    let description = "";
+</script>
+<h1>Weather App</h1>
+
+<form on:submit|preventDefault={getWeather}>
+    <label for="location">Location:</label>
+    <input id="location" bind:value={location} type="text" />
+    <button type="submit">Check</button>
+</form>
